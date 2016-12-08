de7425a73bfb682fb144413cf0dcf5ce642ad051
Ashkgp <vshlkumar56@gmail.com>
2016-12-09 01:32:36 +0530

    Add Seperate Trajectory class

---
+++ trajectory.h
@@ -0,0 +1,70 @@
+#ifndef TRAJECTORY_H
+#define TRAJECTORY_H
+
+#include <fstream>
+#include <opencv2/opencv.hpp>
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+
+struct TransformParam
+{
+    TransformParam() {}
+    TransformParam(double _dx, double _dy, double _da) {
+        dx = _dx;
+        dy = _dy;
+        da = _da;
+    }
+
+    double dx;
+    double dy;
+    double da; // angle
+};
+
+struct Trajectory
+{
+    Trajectory() {}
+    Trajectory(double _x, double _y, double _a) {
+        x = _x;
+        y = _y;
+        a = _a;
+    }
+
+	friend Trajectory operator+(const Trajectory &c1,const Trajectory  &c2){
+		return Trajectory(c1.x+c2.x,c1.y+c2.y,c1.a+c2.a);
+	}
+
+	friend Trajectory operator-(const Trajectory &c1,const Trajectory  &c2){
+		return Trajectory(c1.x-c2.x,c1.y-c2.y,c1.a-c2.a);
+	}
+
+	friend Trajectory operator*(const Trajectory &c1,const Trajectory  &c2){
+		return Trajectory(c1.x*c2.x,c1.y*c2.y,c1.a*c2.a);
+	}
+
+	friend Trajectory operator/(const Trajectory &c1,const Trajectory  &c2){
+		return Trajectory(c1.x/c2.x,c1.y/c2.y,c1.a/c2.a);
+	}
+
+	Trajectory operator =(const Trajectory &rx){
+		x = rx.x;
+		y = rx.y;
+		a = rx.a;
+		return Trajectory(x,y,a);
+	}
+
+    double x;
+    double y;
+    double a; // angle
+};
+
+const int SMOOTHING_RADIUS = 5; // In frames. The larger the more stable the video, but less reactive to sudden panning
+const int HORIZONTAL_BORDER_CROP = 20; // In pixels. Crops the border to reduce the black borders from stabilisation being too noticeable.
+
+// For further analysis
+ofstream out_transform("prev_to_cur_transformation.txt");
+ofstream out_trajectory("trajectory.txt");
+ofstream out_smoothed_trajectory("smoothed_trajectory.txt");
+ofstream out_new_transform("new_prev_to_cur_transformation.txt");
+
+#endif
